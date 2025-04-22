70e203fe592d5b394f54372a9de18531fe29de92
Andrei Popovici <andrei-cristian.popovici@student.upt.ro>
2025-04-22 11:58:26 +0300

    added vscode connection materials

--- AtomC_Compiler_Project.CLI/VSCodeConnection.cs
+++ AtomC_Compiler_Project.CLI/VSCodeConnection.cs
@@ -2,4 +2,29 @@
 
 public class VSCodeConnection
 {
+    private static readonly string[] SupportedExtensions=  { ".atomc", ".c" ,".txt"};
+
+    public static string GetSourceCode()
+    {
+        string baseDirectory =Directory.GetCurrentDirectory();
+        var sourceFile = Directory
+            .EnumerateFiles(baseDirectory)
+            .FirstOrDefault(f => SupportedExtensions.Contains(Path.GetExtension(f)));
+
+        if (sourceFile == null)
+        {
+            Console.Error.WriteLine("Source file not found");
+            Environment.Exit(1);
+        }
+        return File.ReadAllText(sourceFile);
+    }
+    static void Main()
+    {
+        string code = VSCodeConnection.GetSourceCode();
+
+        Console.WriteLine("=== AtomC Source ===");
+        Console.WriteLine(code);
+        Console.WriteLine("====================");
+
+    }
 }
